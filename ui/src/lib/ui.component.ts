869e40e6019aceebf5303cb808023d0920967aeb
Kyle Banks <kyle.banks@breadfinancial.com>
2023-12-20 20:48:59 -0600

    npx nx generate @schematics/angular:library --name=ui --prefix=volt --no-interactive

---
+++ ui/src/lib/ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'volt-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class UiComponent {
+
+}
