b72a61336202478c493f8123536609132d8bfad9
Kyle Banks <kyle.banks@breadfinancial.com>
2023-12-29 11:00:53 -0600

    add game board component

--- ui/src/lib/game-board/game-board.component.ts
+++ ui/src/lib/game-board/game-board.component.ts
@@ -1,12 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TetriminoComponent } from '../tetrimino/tetrimino.component';
 
 @Component({
   selector: 'volt-game-board',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CdkDropList, CdkDropListGroup, TetriminoComponent],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameBoardComponent {}
+export class GameBoardComponent {
+  tray = ['blue', 'blue', 'blue', 'blue', 'red', 'red', 'red', 'red'];
+  board = Array.from({ length: 100 }, () => []);
+
+  drop(event: CdkDragDrop<any>) {
+    console.log(event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
--- ui/src/public-api.ts
+++ ui/src/public-api.ts
@@ -1,8 +1,2 @@
-/*
- * Public API Surface of ui
- */
-
-export * from './lib/ui.service';
-export * from './lib/ui.component';
-
 export * from './lib/game-board/game-board.component';
+export * from './lib/tetrimino/tetrimino.component';
