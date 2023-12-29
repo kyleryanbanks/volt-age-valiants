ed6261b9fb10450c7a6ca943c592534f5af9b3ae
Kyle Banks <kyle.banks@breadfinancial.com>
2023-12-29 11:00:14 -0600

    update app component

--- apps/volt-age-valiants/src/app/app.component.ts
+++ apps/volt-age-valiants/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GameBoardComponent, TetriminoComponent } from '@volt/ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [GameBoardComponent, TetriminoComponent, RouterModule],
   selector: 'volt-age-valiants-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
