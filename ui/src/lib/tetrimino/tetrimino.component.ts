ae8ac82e2a4e14263b7f9dda7dd538082b725f05
Kyle Banks <kyle.banks@breadfinancial.com>
2024-01-01 03:08:34 -0600

    add rotation click handling

--- ui/src/lib/tetrimino/tetrimino.component.ts
+++ ui/src/lib/tetrimino/tetrimino.component.ts
@@ -9,6 +9,21 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tetrimino.component.css',
 })
 export class TetriminoComponent {
+  rotation = 0;
+
   @Input() pieceClass = 'blue';
   @Input() size: number = 1;
+
+  onClick(): void {
+    this.rotation += 90;
+  }
+
+  getRotationStyle(): string {
+    return `rotate(${this.rotation}deg)`;
+  }
+
+  onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+    this.rotation -= 90;
+  }
 }
